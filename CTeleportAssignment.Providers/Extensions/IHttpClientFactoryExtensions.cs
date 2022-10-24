623b188bae79a22cf8797ab5510d7df93133d659
unknown <ra1000ishere@outlook.com>
2022-10-24 10:18:45 +0400

    IHttpClientFactoryExtensions hardcode Get method parameterizated

--- CTeleportAssignment.Providers/Extensions/IHttpClientFactoryExtensions.cs
+++ CTeleportAssignment.Providers/Extensions/IHttpClientFactoryExtensions.cs
@@ -4,7 +4,7 @@
     {
         public static async Task<HttpResponseMessage> SendAsync(this IHttpClientFactory httpClientFactory , string url, HttpMethod method)
         {
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            var httpRequestMessage = new HttpRequestMessage(method, url);
 
             var httpClient = httpClientFactory.CreateClient();
 
